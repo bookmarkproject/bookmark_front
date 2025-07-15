3ff2df54119a7a2eef71db9d6ddb0d94f96f9cd7
Lse-Yeong <ocdee39@gmail.com>
2025-07-15 12:24:10 +0900

    feat: 유틸용 로그아웃 버튼 생성

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:bookmarkfront/provider/auth_provider.dart';
 import 'package:bookmarkfront/provider/member_provider.dart';
 import 'package:bookmarkfront/widgets/app_bars.dart';
+import 'package:bookmarkfront/widgets/custom_filled_button.dart';
+import 'package:bookmarkfront/widgets/custom_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,7 +20,20 @@ class Home extends StatelessWidget {
           child: SafeArea(
             child: Column(
               children: [
-                Text("${member!.nickname}님 환영합니다.")
+                Text("${member!.nickname}님 환영합니다."),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomFilledButton(
+                  callback: (){
+                    Provider.of<AuthProvider>(context, listen: false).clearToken();
+                    showSnack(context, "로그아웃 되었습니다.");
+                    Navigator.pushNamed(context, "/login");
+                  }, 
+                  text: "로그아웃", 
+                  fontsize: 14, 
+                  width: 361
+                )
               ],
             ),
           ),
