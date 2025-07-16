a85c840ee17b1042bfc84e9d578e33aff64f0ecf
Lse-Yeong <ocdee39@gmail.com>
2025-07-16 10:47:49 +0900

    feat: refresh 토큰 프로바이더 추가 및 기능 구현

--- lib/api/utils/api_basic_util.dart
+++ lib/api/utils/api_basic_util.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:bookmarkfront/api/auth_api.dart';
+import 'package:bookmarkfront/widgets/custom_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:bookmarkfront/provider/auth_provider.dart';
 import 'package:provider/provider.dart';
@@ -14,6 +16,25 @@ Map<String,String> getHeadersIncludeAuth(BuildContext context) {
 
 Uri toPasredUrl(String url) {
   return Uri.parse(url);
+}
+
+Future<void> refresh(BuildContext context) async{
+  String? token = Provider.of<AuthProvider>(context,listen: false).refreshToken;
+  final request = {"refreshToken":token};
+  print(token);
+  final newTokens = await refreshRequestToServer(context, request);
+  if(newTokens!=null) {
+    await Provider.of<AuthProvider>(context,listen: false).saveToken(newTokens["accessToken"]!);
+    await Provider.of<AuthProvider>(context,listen: false).saveRefreshToken(newTokens["refreshToken"]!);
+    print("새로운 토큰 저장소에 저장 완료");
+  }
+  else {
+    await Provider.of<AuthProvider>(context,listen: false).clearToken();
+    await Provider.of<AuthProvider>(context,listen: false).clearRefreshToken();
+    showSnack(context, "다시 로그인 해주세요.",isError: true);
+    print("현재 저장된 토큰 : ${Provider.of<AuthProvider>(context,listen: false).accessToken}");
+    Navigator.pushNamed(context, '/login');
+  }
 }
 
 String errorMessage(response) {
